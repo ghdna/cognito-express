ff055a3b110d28c38004835471b11efa3f10ca51
ghdna <git@33dna.33mail.com>
2017-12-11 20:15:33 -0600

    refactoring

--- test/strategy.js
+++ test/strategy.js
@@ -1,8 +1,10 @@
 "use strict";
 
-const expect = require("chai").expect,
+const chai = require("chai"),
+	chaiAsPromised = require("chai-as-promised"),
 	Strategy = require(".."),
 	rewire = require("rewire"),
+	expect = chai.expect,
 	strategyConfig = {
 		region: "us-east-1",
 		cognitoUserPoolId: "us-east-1_dXlFef73t",
@@ -11,6 +13,8 @@ const expect = require("chai").expect,
 	},
 	token =
 		"eyJraWQiOiJVb0dmOG95UTNrbXB6WlZFWmZxS3RDQ0hIQ3lVNUo3RnV6dFRWSmxRNGtZPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzNzZkOWY5MC04MjkzLTRhODMtYjA0Yi0zYWRjZTJlYTkzZTAiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfZFhsRmVmNzN0IiwiZXhwIjoxNTAzNDA5NjM5LCJpYXQiOjE1MDM0MDYwMzksImp0aSI6ImE1YjhjNWZhLWJiNDgtNGNkYi1hOTZiLTk4MTc3NjEyN2I1NyIsImNsaWVudF9pZCI6IjZ1ZDZ1cG9xaG5kYzd2Y3YxMDR2bG5hbHBuIiwidXNlcm5hbWUiOiJ2aXNhb2ZmaWNlcjFAY25hLmdvdiJ9.FL69JLtHFCeBU1nKYRyZYBBEO1TVaDCCuRTvrw_piSqLM_DQ2sUUJQYr9Ww3DudaTcjHDkV_rJufb3BzAfVMG0pOkZit6JkTexoCOHzXRNn0Bgmk87vVB52H2zOe6hRw_pF90UUmg6q9kYb2zapQg_-3ZvE1TiDQ0v45RZDKUHqWUFka3L3q6_uDHfCFrTr_APBN7y2u_w10jaL75CQ4h3RePOxKTiySjLq0FsFnl576eu_ZkoliUmApPCD618dRbu4kybCizbvYLLx8x-vM3CvPjnv_bXL1FtOWjzjSifuM5Hjnr1FxozCBQipBZ7X6vQBKy5hNX17O3sBt9KXXlg";
+
+chai.should();
 
 describe("Strategy Negative Scenarios", () => {
 	it("should not have config undefined", function() {
@@ -71,6 +75,7 @@ describe("Strategy Negative Scenarios", () => {
 			});
 		}).to.throw(TypeError, "AWS Region not specified in constructor");
 	});
+
 });
 
 describe("Strategy Positive Scenarios", () => {
